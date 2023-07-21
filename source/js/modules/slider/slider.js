78d7e7dfb3fc3744738dc370f366dda9a07b389e
[Georgezho] <[johimichev@gmail.com]>
2023-07-22 01:35:05 +0300

    завершает блок 'Тренеры' и частично завершен блок 'Наши преимущества'

--- source/js/modules/slider/slider.js
+++ source/js/modules/slider/slider.js
@@ -1,18 +1,30 @@
 const swiper = new Swiper('.swiper', {
   // Настройки слайдера
   slidesPerView: 4,
-  spaceBetween: 30,
+  spaceBetween: 40,
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
   },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
 
